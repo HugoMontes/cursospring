df969bb1344419c1dddba8e59334c853dfdc2980
Hugo Montes <hmontes@cofar.com.bo>
2018-05-02 09:53:57 -0400

    Se adiciono validacion para formulario de persona

--- src/main/java/com/educomser/cursospring/controller/PersonaController.java
+++ src/main/java/com/educomser/cursospring/controller/PersonaController.java
@@ -4,12 +4,14 @@ import com.educomser.cursospring.model.Persona;
 import com.educomser.cursospring.service.EjemploService;
 import java.util.ArrayList;
 import java.util.List;
+import javax.validation.Valid;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,10 +23,10 @@ import org.springframework.web.servlet.view.RedirectView;
 @Controller
 @RequestMapping("/persona")
 public class PersonaController {
-    
+
     // Definir un atributo de tipo Log
-    private static final Log LOGGER=LogFactory.getLog(PersonaController.class);
-    
+    private static final Log LOGGER = LogFactory.getLog(PersonaController.class);
+
     // Inyectar el servicio ejemploService
     @Autowired
     @Qualifier("ejemploService")
@@ -44,7 +46,7 @@ public class PersonaController {
         mv.addObject("persona", new Persona("Maria", 21));
         return mv;
     }
-    
+
     // Retornando objeto Model And View
     @GetMapping("/listado")
     public ModelAndView listado() {
@@ -52,55 +54,64 @@ public class PersonaController {
         mv.addObject("personas", listadoPersonas());
         return mv;
     }
-    
-    private List<Persona> listadoPersonas(){
-        List<Persona> lista=new ArrayList<Persona>();
+
+    private List<Persona> listadoPersonas() {
+        List<Persona> lista = new ArrayList<Persona>();
         lista.add(new Persona("Marcos", 42));
         lista.add(new Persona("Lucas", 33));
         lista.add(new Persona("Jesus", 16));
         lista.add(new Persona("Ana", 28));
         return lista;
     }
-    
+
     @GetMapping("/formulario")
-    public String mostrarFormulario(Model model){
-        model.addAttribute("persona",new Persona());
+    public String mostrarFormulario(Model model) {
+        model.addAttribute("persona", new Persona());
         return "persona/formulario";
     }
-    
+
+    // @Valid: Indica que se tiene que validar con las anotaciones en el modelo
+    // BindingResult: Encapsula los errores para enviarselos al formulario
     @PostMapping("/adicionar")
-    public ModelAndView addPersona(@ModelAttribute("persona") Persona persona){
-        // Log que muestra el metodo ejecutado y los parametros recibidos
-        LOGGER.info("METHOD: addPersona | PARAMS: "+persona);
-        ModelAndView mv=new ModelAndView("persona/resultado");
-        mv.addObject("persona",persona);
-        // Log que muestra la plantilla y los datos enviados a la misma
-        LOGGER.info("TEMPLATE: persona/resultado | DATA: "+persona);
+    public ModelAndView addPersona(@Valid @ModelAttribute("persona") Persona persona,
+            BindingResult bindingResult) {
+        LOGGER.info("METHOD: addPersona | PARAMS: " + persona);
+        // Instanciar un ModelAndView
+        ModelAndView mv = new ModelAndView("persona/resultado");
+        // Verificar que cumplan las condiciones en el modelo
+        if (bindingResult.hasErrors()) {
+            mv.setViewName("persona/formulario");
+        } else {
+            mv.setViewName("persona/resultado");
+            mv.addObject("persona", persona);
+        }
+        // Obtener el nombre dinamicamente de la plantilla
+        LOGGER.info("TEMPLATE: " + mv.getViewName() + " | DATA: " + persona);
         return mv;
     }
-    
+
     // Primera forma
     @GetMapping("/redireccionar1")
-    public String redirect1(){
+    public String redirect1() {
         return "redirect:/persona/formulario";
     }
-    
+
     // Segunda forma    
     @GetMapping("/redireccionar2")
-    public RedirectView redirect2(){
+    public RedirectView redirect2() {
         return new RedirectView("/persona/listado");
     }
-    
+
     // Generar error 500    
     @GetMapping("/dividir")
-    public RedirectView dividir(){
-        int i=6/0;
+    public RedirectView dividir() {
+        int i = 6 / 0;
         return new RedirectView("/persona/formulario");
     }
-    
+
     @GetMapping("/saludar/{nom}")
-    public ModelAndView saludar(@PathVariable("nom") String nombre){
-        ModelAndView mv=new ModelAndView("/persona/servicio");
+    public ModelAndView saludar(@PathVariable("nom") String nombre) {
+        ModelAndView mv = new ModelAndView("/persona/servicio");
         // Llamar al metodo del servicio de negocio
         mv.addObject("mensaje", ejemploService.saludar(nombre));
         return mv;
